b0b584cfaa20e413d723fc1d8439353fb243141f
Mark Martynov <mark.martynov@team.wrike.com>
2018-01-29 11:29:45 +0300

    editable title component added

--- list/lib/src/task_list/task_card/task_card_component.dart
+++ list/lib/src/task_list/task_card/task_card_component.dart
@@ -1,5 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:list/src/core_components/directives/hover_hooks.dart';
+import 'package:list/src/core_components/editable_title/editable_title.dart';
+import 'package:list/src/core_components/editable_title/title_model.dart';
 import 'package:list/src/task_list/core/task_list_model.dart';
 
 @Component(
@@ -8,10 +10,22 @@ import 'package:list/src/task_list/core/task_list_model.dart';
   templateUrl: 'task_card_component.html',
   directives: const <Object>[
     CORE_DIRECTIVES,
-    HoverHooks
+    HoverHooks,
+    EditableTitle
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 )
-class TaskCardComponent {
+class TaskCardComponent implements AfterViewInit, OnChanges {
   @Input() TaskListModel model;
+
+
+  TitleModel titleModel;
+
+  @override
+  void ngAfterViewInit() {}
+
+  @override
+  void ngOnChanges(Map<String, SimpleChange> changes) {
+    titleModel = new TitleModel(model.text);
+  }
 }
